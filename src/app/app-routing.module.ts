6b5a8ca97c4a003c60b39dbb8ef757b076e34492
Ivan Enrique Córdova Gómez <1802408@utrivieramaya.edu.mx>
2023-07-24 17:00:40 -0500

    0.21
    correciones a la tabla del dashboard tabla 2
    se agrego la vista del calendario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 import { LoginComponent } from './login/login.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: PrincipalComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'calendario', component: CalendarioComponent, pathMatch: 'full' },
 
 ];
 
